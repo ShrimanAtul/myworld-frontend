b0cb4f4d09bb22cee2954b2a261b13388023f8a8
ShrimanAtul <atulforce@gmail.com>
2026-03-05 01:40:03 +0530

    replacing with actual Render deployed backend app urls

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -1,12 +1,22 @@
 const getApiUrl = (): string => {
   const env = import.meta.env.VITE_APP_ENV || 'local';
   
+  // const urls: Record<string, string> = {
+  //   local: 'http://localhost:8080',
+  //   dev: 'https://dev.api.myworld.ai',
+  //   uat: 'https://uat.api.myworld.ai',
+  //   perf: 'https://perf.api.myworld.ai',
+  //   prod: 'https://api.myworld.ai',
+  // };
+  
+  // Commented above as replacing with actual Render deployed backend app urls.
+  
   const urls: Record<string, string> = {
     local: 'http://localhost:8080',
-    dev: 'https://dev.api.myworld.ai',
-    uat: 'https://uat.api.myworld.ai',
-    perf: 'https://perf.api.myworld.ai',
-    prod: 'https://api.myworld.ai',
+    dev: 'https://myworld-backend-md68.onrender.com',
+    uat: 'https://myworld-backend-md68.onrender.com',
+    perf: 'https://myworld-backend-md68.onrender.com',
+    prod: 'https://myworld-backend-md68.onrender.com',
   };
 
   return urls[env] || urls.local;
